d5e8ae83abd1d0880e4aa76da694d734eb4dccb0
Henrique Fernandes <henrique.contato@outlook.com>
2016-04-13 12:52:11 -0300

    Adding some boilerplate.

--- device_sensors/app.js
+++ device_sensors/app.js
@@ -1,1 +1,13 @@
-// TODO
+var express = require('express');
+var r = require('rethinkdb');
+
+var app = express();
+var appPort = 3000;
+
+app.get('/', function (req, res) {
+     res.send('Hello Device!');
+});
+
+app.listen(appPort, function () {
+    console.log('Were are live, listening on port 3000!');
+});
