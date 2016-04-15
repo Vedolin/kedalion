b0bbc687a9aa070bff3da4bcd704a75b31783055
Henrique Fernandes <henrique.contato@outlook.com>
2016-04-14 22:21:40 -0300

    Ajusts

--- device_sensors/app.js
+++ device_sensors/app.js
@@ -8,6 +8,17 @@ app.get('/', function (req, res) {
      res.send('Hello Device!');
 });
 
+app.get('/device_sensores_package', function (req, res) {
+    var connect_db = r.connect({
+        host: 'localhost',
+        port: 28015,
+        db: 'device_sensores_db',
+    })
+    
+    // r.db("device_sensores_db").table("device_sensores_package")
+     res.send('Hello Device!');
+});
+
 app.listen(appPort, function () {
     console.log('Were are live, listening on port 3000!');
 });
--- device_sensors/create_data_base.js
+++
@@ -1,8 +0,0 @@
-var r = require('rehinkdb');
-
-var connect_db = r.connect({
-    host: 'localhost',
-    port: 28015,
-    db: 'device_sensores_db',
-
-})
